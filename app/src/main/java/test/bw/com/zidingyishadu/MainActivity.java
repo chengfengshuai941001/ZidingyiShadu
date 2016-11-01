16d5a96e4138e4cb9769e6eded8c44b4738e16b8
chengfengshuai941001 <746090462@qq.com>
2016-11-01 21:08:37 +0800

    Initial commit

---
+++ app/src/main/java/test/bw/com/zidingyishadu/MainActivity.java
@@ -0,0 +1,18 @@
+package test.bw.com.zidingyishadu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
